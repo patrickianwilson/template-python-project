b9342ebd703a93df400391ba304f8307136c32d9
pwilson <pwilson@inquestdevops.com>
2024-01-29 07:31:49 -0800

    adding cassus deployment definition to template

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.buildFeatures.dockerSupport
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.DockerCommandStep
 import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCommand
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
@@ -8,8 +8,11 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.buildSteps.GradleBuildStep
 import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
 
-import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
 import jetbrains.buildServer.configs.kotlin.triggers.finishBuildTrigger
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.ui.add
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -35,20 +38,21 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 
 version = "2021.2"
 
-object BuildConstants{
-    const val DOCKER_REGISTRY_ID = "PROJECT_EXT_5"
-}
-
-
 project {
 
     buildType(Build)
-    buildType(Release)
+    buildType(CreateInfra)
+//    buildType(IntTestDev)
+//    vcsRoot(IntTestVcsRoot)
+    buildType(DeployDev)
+    buildType(DeployProd)
 }
 
 object Build : BuildType({
     name = "Build"
-    artifactRules = "build/**/* => build_artifacts"
+    maxRunningBuilds = 5
+    artifactRules = "+:build/**/* => build_artifacts,+:src/main/deploy/Dockerfile => build_artifacts,+:.cassius/Application.json => build_artifacts"
+
     vcs {
         root(DslContext.settingsRoot)
     }
@@ -74,33 +78,43 @@ object Build : BuildType({
                 dockerRegistryId = "PROJECT_EXT_5"
             }
         }
-        commitStatusPublisher {
-            vcsRootExtId = "${DslContext.settingsRoot.id}"
-            publisher = space {
-                authType = connection {
-                    connectionId = "PROJECT_EXT_2"
-                }
-                displayName = "TeamCity"
-            }
-        }
     }
 })
 
-object Release : BuildType({
-    name = "Release"
+object CreateInfra : BuildType({
+    name = "CreateEnvironments"
+
     maxRunningBuilds = 1
     type = BuildTypeSettings.Type.DEPLOYMENT
     enablePersonalBuilds = false
-    vcs {
-        root(DslContext.settingsRoot)
+    artifactRules = "**/* => build_artifacts"
+    params {
+        param("PATH", "${'$'}CONTAINER_PATH:${'$'}PATH")
     }
-
     steps {
-        gradle {
-            tasks = "publishAllPublicationsToMavenInternalRepository"
-            useGradleWrapper = false
-            dockerImagePlatform = GradleBuildStep.ImagePlatform.Linux
+
+        script {
+            name = "Cassius Infra Creation"
+            /* FIXME */
+            scriptContent = """
+                echo "Environment For Build Num: ${'$'}{BUILD_NUMBER}"
+                env
+                
+                echo "Current Directory Contents"
+                ls -al
+                                    
+                echo "Authenticate GCloud"
+                gcloud auth activate-service-account --key-file ${'$'}GOOGLE_APPLICATION_CREDENTIALS
+                                    
+                echo "GCloud Tool Config"
+                gcloud config list
+                
+                echo "Importing Image Into Cassius"
+                cassius environment patch --appId %%{{ModuleName}}%% --configFile Application.json
+            """.trimIndent()
             dockerImage = "inquest.registry.jetbrains.space/p/buildtools/buildimages/buildimage:latest"
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+            dockerPull = true
         }
     }
 
@@ -120,6 +134,240 @@ object Release : BuildType({
 
     dependencies {
         dependency(Build) {
+            snapshot {
+                onDependencyFailure = FailureAction.CANCEL
+                onDependencyCancel = FailureAction.CANCEL
+            }
+
+            artifacts {
+                artifactRules = "build_artifacts/**"
+            }
+        }
+    }
+})
+
+object DeployDev : BuildType({
+    name = "Deploy to DEV"
+
+    maxRunningBuilds = 1
+    type = BuildTypeSettings.Type.DEPLOYMENT
+    enablePersonalBuilds = false
+    artifactRules = "**/* => build_artifacts"
+    params {
+        param("PATH", "${'$'}CONTAINER_PATH:${'$'}PATH")
+    }
+    steps {
+        dockerCommand {
+            name = "Build Docker Image"
+            commandType = build {
+                source = file {
+                    path = "Dockerfile"
+                }
+                contextDir = "."
+                platform = DockerCommandStep.ImagePlatform.Linux
+                namesAndTags = """
+                    %%{{ModuleName.lowerCase}}%%:%build.number%
+                    inquest.registry.jetbrains.space/p/buildtools/services/%%{{ModuleName.lowerCase}}%%:1.0.0.%build.number%
+                    inquest.registry.jetbrains.space/p/buildtools/services/%%{{ModuleName.lowerCase}}%%:latest
+                """.trimIndent()
+                commandArgs = "--pull"
+            }
+        }
+        dockerCommand {
+            name = "Push Docker Image"
+            executionMode = BuildStep.ExecutionMode.RUN_ON_SUCCESS
+            commandType = push {
+                /* FIXME */
+                namesAndTags = """
+                   inquest.registry.jetbrains.space/p/buildtools/services/%%{{ModuleName.lowerCase}}%%:1.0.0.%build.number%
+                    inquest.registry.jetbrains.space/p/buildtools/services/%%{{ModuleName.lowerCase}}%%:latest
+                """.trimIndent()
+            }
+        }
+        script {
+            name = "Cassius Import"
+            /* FIXME */
+            scriptContent = """
+                echo "Environment For Build Num: ${'$'}{BUILD_NUMBER}"
+                env
+                
+                echo "Current Directory Contents"
+                ls -al
+                                    
+                echo "Authenticate GCloud"
+                gcloud auth activate-service-account --key-file ${'$'}GOOGLE_APPLICATION_CREDENTIALS
+                                    
+                echo "GCloud Tool Config"
+                gcloud config list
+                
+                echo "Importing Image Into Cassius"
+                cassius deployment-bundle import --deploymentBundleName %%{{ModuleName.lowerCase}}%%-dev --version=1.0.0.${'$'}BUILD_NUMBER --image=inquest.registry.jetbrains.space/p/buildtools/services/%%{{ModuleName.lowerCase}}%%:1.0.0.${'$'}BUILD_NUMBER
+                
+                echo "Deploying Version to Dev Application Environment"
+                cassius environment deploy --deploymentBundleVersion=1.0.0.${'$'}BUILD_NUMBER --appId %%{{ModuleName}}%% --envName DEV
+            """.trimIndent()
+            dockerImage = "inquest.registry.jetbrains.space/p/buildtools/buildimages/buildimage:latest"
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+            dockerPull = true
+        }
+    }
+
+    triggers {
+        finishBuildTrigger {
+            buildType = "${CreateInfra.id}"
+            successfulOnly = true
+        }
+    }
+    features {
+        dockerSupport {
+            loginToRegistry = on {
+                dockerRegistryId = "PROJECT_EXT_5"
+            }
+        }
+    }
+
+    dependencies {
+        dependency(CreateInfra) {
+            snapshot {
+                onDependencyFailure = FailureAction.CANCEL
+                onDependencyCancel = FailureAction.CANCEL
+            }
+
+            artifacts {
+                artifactRules = "build_artifacts/**"
+            }
+        }
+    }
+})
+
+//object IntTestVcsRoot: GitVcsRoot({
+//    /* FIXME */
+//
+//    id("SshGitGitJetbrainsSpaceInquestBuildtoolsModule%%{{ModuleName}}%%IntegTestsGitRefsHeadsMaster")
+//    name = "ssh://git@git.jetbrains.space/inquest/buildtools/Module-%%{{ModuleName}}%%IntegTests.git#refs/heads/master"
+//    url = "ssh://git@git.jetbrains.space/inquest/buildtools/Module-%%{{ModuleName}}%%IntegTests.git"
+//    branch = "refs/heads/master"
+//    branchSpec = "refs/heads/*"
+//    authMethod = uploadedKey {
+//        userName = "git"
+//        uploadedKey = "SpaceSshKey"
+//    }
+//})
+//
+//object IntTestDev: BuildType({
+//
+//    name = "IntTestDev"
+//    artifactRules = "+:build/**/* => build_artifacts,+:src/main/deploy/Dockerfile => build_artifacts"
+//    vcs {
+//        /* FIXME */
+//        root(RelativeId("SshGitGitJetbrainsSpaceInquestBuildtoolsModule%%{{ModuleName}}%%IntegTestsGitRefsHeadsMaster"))
+//    }
+//
+//    steps {
+//        gradle {
+//            tasks = "test"
+//            useGradleWrapper = false
+//            dockerImagePlatform = GradleBuildStep.ImagePlatform.Linux
+//            dockerPull = true
+//            dockerImage = "inquest.registry.jetbrains.space/p/buildtools/buildimages/buildimage:latest"
+//        }
+//      script {
+//            name = "Set Deployment Status Test"
+//            id = "Set_Deployment_Status_Test"
+//            scriptContent = """
+//              DEPLOYMENT_ATTEMPT=${'$'}(cat .cassius/last-deployment-attempt-id)
+//              echo "Setting Status for Deployment: ${'$'}{DEPLOYMENT_ATTEMPT}"
+//              cassius environment set-deployment-status --appId %%{{ModuleName}}%% --envName DEV --deploymentAttemptId ${'$'}{DEPLOYMENT_ATTEMPT} --succeeded
+//            """.trimIndent()
+//      }
+//    }
+
+//
+//    triggers {
+//        vcs {
+//        }
+//        finishBuildTrigger {
+//            buildType = "${DeployDev.id}"
+//            successfulOnly = true
+//        }
+//    }
+//
+//    features {
+//        perfmon {
+//        }
+//        dockerSupport {
+//            loginToRegistry = on {
+//                dockerRegistryId = "PROJECT_EXT_5"
+//            }
+//        }
+//    }
+//
+//    dependencies {
+//        dependency(DeployDev) {
+//            snapshot {
+//                onDependencyFailure = FailureAction.CANCEL
+//                onDependencyCancel = FailureAction.CANCEL
+//            }
+//
+//            artifacts {
+//                artifactRules = "build_artifacts/**"
+//            }
+//        }
+//    }
+//})
+
+object DeployProd : BuildType({
+    name = "Deploy to Prod"
+    paused = true
+    maxRunningBuilds = 1
+    type = BuildTypeSettings.Type.DEPLOYMENT
+    enablePersonalBuilds = false
+    params {
+        param("PATH", "${'$'}CONTAINER_PATH:${'$'}PATH")
+    }
+    steps {
+        script {
+            name = "Cassius Deploy"
+            /* FIXME */
+
+            scriptContent = """
+                echo "Environment For Build Num: ${'$'}{BUILD_NUMBER}"
+                env
+                
+                echo "Current Directory Contents"
+                ls -al
+                                    
+                echo "Authenticate GCloud"
+                gcloud auth activate-service-account --key-file ${'$'}GOOGLE_APPLICATION_CREDENTIALS
+                                    
+                echo "GCloud Tool Config"
+                gcloud config list
+                
+                echo "Deploying Version to Dev Application Environment"
+                cassius environment deploy --deploymentBundleVersion=1.0.0.${'$'}BUILD_NUMBER --appId %%{{ModuleName}}%% --envName Prod
+            """.trimIndent()
+            dockerImage = "inquest.registry.jetbrains.space/p/buildtools/buildimages/buildimage:latest"
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+            dockerPull = true
+        }
+    }
+
+    triggers {
+        finishBuildTrigger {
+            buildType = "${DeployDev.id}"
+            successfulOnly = true
+        }
+    }
+    features {
+        dockerSupport {
+            loginToRegistry = on {
+                dockerRegistryId = "PROJECT_EXT_5"
+            }
+        }
+    }
+
+    dependencies {
+        dependency(DeployDev) { //update this to IntTestDev to enable integ testing in DEV prior to Prod deployment
             snapshot {
                 onDependencyFailure = FailureAction.CANCEL
                 onDependencyCancel = FailureAction.CANCEL
--- README.md
+++ README.md
@@ -1,21 +1,109 @@
 # template-python-project
 
-[![PyPI - Version](https://img.shields.io/pypi/v/template-python-project.svg)](https://pypi.org/project/template-python-project)
-[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/template-python-project.svg)](https://pypi.org/project/template-python-project)
+## To Deploy
+This template requires a Google Service Account and project:
 
------
+```bash
+SERVICE_NAME=%%{{ModuleName.lowerCase}}%%
+ENV=dev
+PROJECT=inquest-$SERVICE_NAME-$ENV
+SERV_ACCNT_NAME="cassius-service-account"
+DEV_INGRESS_IP=127.0.0.1  # change me
+DNS_ZONE_NAME='inquest-devops'  # change me
+gcloud projects create $PROJECT
 
-**Table of Contents**
+gcloud beta iam service-accounts create $SERV_ACCNT_NAME \
+    --display-name "Generated service account for Cassius" \
+    --project $PROJECT
+    
+gcloud projects add-iam-policy-binding $PROJECT \
+  --member serviceAccount:$SERV_ACCNT_NAME@$PROJECT.iam.gserviceaccount.com \
+  --role roles/editor
+    
+gcloud iam service-accounts keys create ~/dev-key.json \
+  --iam-account $SERV_ACCNT_NAME@$PROJECT.iam.gserviceaccount.com \
+  --project $PROJECT
 
-- [Installation](#installation)
-- [License](#license)
+ENV=prod
+PROJECT=inquest-$SERVICE_NAME-$ENV
+PROD_INGRESS_IP=127.0.0.1  #change me
+DNS_ZONE_NAME='inquest-devops'  #change me for PROD
+gcloud projects create $PROJECT
 
-## Installation
-
-```console
-pip install template-python-project
+gcloud beta iam service-accounts create $SERV_ACCNT_NAME \
+    --display-name "Generated service account for Cassius" \
+    --project $PROJECT
+    
+gcloud projects add-iam-policy-binding $PROJECT \
+  --member serviceAccount:$SERV_ACCNT_NAME@$PROJECT.iam.gserviceaccount.com \
+  --role roles/editor
+    
+gcloud iam service-accounts keys create ~/prod-key.json \
+  --iam-account $SERV_ACCNT_NAME@$PROJECT.iam.gserviceaccount.com \
+  --project $PROJECT
+ 
 ```
 
-## License
+Note the location of the key (for me it is ~/key.json)
 
-`template-python-project` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
+```bash
+cassius secret create --secretName %%{{ModuleName.lowerCase}}%%-dev-gcloud-credentials --file ~/dev-key.json
+cassius secret create --secretName %%{{ModuleName.lowerCase}}%%-prod-gcloud-credentials --file ~/prod-key.json
+
+```
+
+## Setup Datastore (if service needs a DB)
+
+Open the following url in a browser
+```
+echo https://console.cloud.google.com/datastore/setup?project=$PROJECT
+```
+
+The enable the datastore API
+
+```bash
+gcloud services enable datastore.googleapis.com --project $PROJECT
+```
+
+## Setup Authentication and Authorization for new service
+
+### (optional) create the service account.
+```bash
+mercury client create --clientId %%{{ModuleName.lowerCase}}%%-dev-svc --grant client_credentials --path http://rabbit-dev.inquestdevops.com/auth/merc/complete
+mercury client create --clientId %%{{ModuleName.lowerCase}}%%-dev-svc --grant client_credentials --path http://rabbit-dev.inquestdevops.com/auth/merc/complete
+```
+**Note:** Take note of the generated service account secret
+
+```bash
+cassius secret create --secretName %%{{ModuleName.lowerCase}}%%-dev-service-account-secret --strContent <string from above>
+cassius secret create --secretName %%{{ModuleName.lowerCase}}%%-dev-service-account-secret --strContent <string from above>
+
+cassius secret create --secretName %%{{ModuleName.lowerCase}}%%-dev-rabbit-admin-token --strContent insecure   #dev admin tokens are not secure
+cassius secret create --secretName %%{{ModuleName.lowerCase}}%%-prod-rabbit-admin-token --strContent <secure password>
+```
+
+### Create a Deployment Bundle for Cassius to Track images
+
+```
+cassius deployment-bundle create --name %%{{ModuleName.lowerCase}}%%-dev --type image
+```
+
+
+### grant permissions needed by your service:
+
+**Eg**: (the following is not actually needed to run a cardinal service)
+
+```
+cardsharp policy create --name "%%{{ModuleName.lowerCase}}%%-integ-testing-perms-policy" \
+--description "A policy granting the integ testing service account access to all APIs and resources for this service" \
+--permission "%%{{ModuleName}}%%.*" \
+--resource "*" \
+--account "%%{{ModuleName.lowerCase}}%%-dev-svc"
+
+```
+
+Finally, you may want to register a user account for testing (not required):
+
+```
+mercury account register --display-name TestUser --email test@inquestdevops.com --fname Test --lname User --password insecure
+```
